312a74681fe9946d3f84ae68d8b34bcfa6152cbf
aryakrishna03 <yapapacat1@gmail.com>
2026-02-28 23:22:31 -0800

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -11,13 +11,37 @@ typedef struct {
 
 int main() {
     int n;
-    printf("Enter number of processes: ");
     scanf("%d", &n);
 
     Process p[n];
 
     for(int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+    }
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(p[i].arrival > p[j].arrival) {
+                Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    int time = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(time < p[i].arrival)
+            time = p[i].arrival;
+
+        p[i].waiting = time - p[i].arrival;
+        time += p[i].burst;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%s %d %d\n", p[i].pid, p[i].waiting, p[i].turnaround);
     }
 
     return 0;
