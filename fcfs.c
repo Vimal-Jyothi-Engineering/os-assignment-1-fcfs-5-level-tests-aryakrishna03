58f1040fff68d56d9c3888d66505ff66a730507e
aryakrishna03 <yapapacat1@gmail.com>
2026-02-28 23:33:01 -0800

    fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -18,8 +18,9 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    for(int i = 0; i < n-1; i++) {
-        for(int j = i+1; j < n; j++) {
+    // sort by arrival
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
             if(p[i].arrival > p[j].arrival) {
                 Process temp = p[i];
                 p[i] = p[j];
@@ -28,19 +29,20 @@ int main() {
         }
     }
 
-    int time = 0;
+    int current_time = 0;
 
     for(int i = 0; i < n; i++) {
-        if(time < p[i].arrival)
-            time = p[i].arrival;
+        if(current_time < p[i].arrival)
+            current_time = p[i].arrival;
 
-        p[i].waiting = time - p[i].arrival;
-        time += p[i].burst;
+        p[i].waiting = current_time - p[i].arrival;
+        current_time += p[i].burst;
         p[i].turnaround = p[i].waiting + p[i].burst;
     }
 
     for(int i = 0; i < n; i++) {
-        printf("%s %d %d\n", p[i].pid, p[i].waiting, p[i].turnaround);
+        printf("%s %d %d", p[i].pid, p[i].waiting, p[i].turnaround);
+        if(i != n-1) printf("\n");
     }
 
     return 0;
